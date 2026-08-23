ffae4e6b90700c118984e91e26b1d80f35487965
Jimena-CC <jimenacondor2@gmail.com>
2026-08-23 16:26:28 -0500

    init(desktop): inicializar proyecto: - Se hizo la inicialización del proyecto utilizando NetBeans. - Se verificó que el proyecto ejecuta y genera un "Hola Mundo".

---
+++ desktop/src/main/java/com/citasmedicas/sistemasc/SistemasC.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.citasmedicas.sistemasc;
+
+/**
+ *
+ * @author jimen
+ */
+public class SistemasC {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
